37209769c7effc8cf11b2d61116794b7beaa7f07
Karl Hepler <karl.hepler@gmail.com>
2019-05-25 00:10:50 -0400

    added a few configs

--- install.sh
+++ install.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
-# Define some local variables
-ROOT=$(dirname $0)
+DOTFILES=$(cd `dirname $0` && pwd)
+
+# Take ownership of /usr/local files
+sudo chown -R $USER:admin /usr/local/*
 
 # Install OSX command line tools
-# if the output of "xcode-select -p" is not an existing directory
-if [ ! -d "$(xcode-select -p)" ]; then
-  xcode-select --install
-fi
+xcode-select --install
 
-# Install Homebrew and packages
-mkdir -p $HOME/brew/usr/local && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/brew/usr/local
-xargs $HOME/brew/usr/local/bin/brew install < $ROOT/homebrew/brew.formulae
-xargs $HOME/brew/usr/local/bin/brew cask install < $ROOT/homebrew/cask.formulae
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install CLI formulae
+/usr/local/bin/brew install \
+	git \
+	zsh \
+	zsh-completions \
+	ripgrep \
+	fzf \
+	vim \
+	go \
+	nvm
+
+# Install GUI formulae
+/usr/local/bin/brew cask install \
+	iterm2 \
+	google-chrome \
+	karabiner-elements
+
+# Install source code pro font
+/usr/local/bin/brew tap homebrew/cask-fonts && brew cask install font-source-code-pro
 
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Replace default configs with symlinks
+rm -rf $HOME/.config/karabiner && ln -s $DOTFILES/karabiner $HOME/.config/karabiner && launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
+rm -f $HOME/{.zshenv,.zprofile,.zshrc,.zlogin,.zlogout} && ln -s $DOTFILES/zsh/{zshenv,zprofile,zshrc,zlogin,zlogout} $HOME/{.zshenv,.zprofile,.zshrc,.zlogin,.zlogout} 
+rm -rf $HOME/.vim && ln -s $DOTFILES/vim $HOME/.vim
+rm -f $HOME/.gitignore && ln -s $DOTFILES/git/gitignore
+rm -f $HOME/.gitconfig && ln -s $DOTFILES/git/gitconfig
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
