634de9076b86d6b75831b44cb5c7f3edfd8b6bda
Karl Hepler <karl.hepler@gmail.com>
2019-05-25 00:26:42 -0400

    fixed zsh symlinks

--- install.sh
+++ install.sh
@@ -36,10 +36,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Replace default configs with symlinks
 rm -rf $HOME/.config/karabiner && ln -s $DOTFILES/karabiner $HOME/.config/karabiner && launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
-rm -f $HOME/{.zshenv,.zprofile,.zshrc,.zlogin,.zlogout} && ln -s $DOTFILES/zsh/{zshenv,zprofile,zshrc,zlogin,zlogout} $HOME/{.zshenv,.zprofile,.zshrc,.zlogin,.zlogout} 
+rm -f $HOME/{.zshenv,.zprofile,.zshrc,.zlogin,.zlogout} && ln -s $DOTFILES/zsh/zshenv $HOME/.zshenv && ln -s $DOTFILES/zsh/zprofile $HOME/.zprofile && ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc && ln -s $DOTFILES/zsh/zlogin $HOME/.zlogin && ln -s $DOTFILES/zsh/zlogout $HOME/.zlogout
 rm -rf $HOME/.vim && ln -s $DOTFILES/vim $HOME/.vim
-rm -f $HOME/.gitignore && ln -s $DOTFILES/git/gitignore
-rm -f $HOME/.gitconfig && ln -s $DOTFILES/git/gitconfig
+rm -f $HOME/.gitignore && ln -s $DOTFILES/git/gitignore $HOME/.gitignore
+rm -f $HOME/.gitconfig && ln -s $DOTFILES/git/gitconfig $HOME/.gitconfig
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
